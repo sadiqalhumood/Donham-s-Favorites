77ddd97cc8c21f1ec5dfcc80d3ad8e748bcd282a
suarezjw123 <jwsuarez@bu.edu>
2023-04-27 11:37:07 -0400

    Database works !

---
+++ MAIN/Assets/Plugins/SQLite4Unity3d/Scripts/Person.cs
@@ -0,0 +1,15 @@
+﻿using SQLite4Unity3d;
+using TMPro;
+
+public class Person  {
+
+	[PrimaryKey, AutoIncrement]
+	public int Id { get; set; }
+	public string Username { get; set; }
+	public string Password { get; set; }
+
+	public override string ToString ()
+	{
+		return string.Format ("[Person: Id={0}, Name={1},  Surname={2}]", Id, Username, Password);
+	}
+}
